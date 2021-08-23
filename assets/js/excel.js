1a9a3a92a76712d9d068c03d0165933bc6fc4eb9
John Andrés Camacho <jcamacho@hifebo.com>
2021-08-23 15:41:57 -0500

    Regular commit

--- assets/js/excel.js
+++ assets/js/excel.js
@@ -1,63 +1,122 @@
-$.get( "./assets/php/listado.php", ( data ) => {
-    let cont = 1;
-    for (const item of data) {
-        const element = '<div class="product-item"><div class="product-item__thumb"><a href="detail.html?id='+item.id+'"><img class="thumb-primary" src="./assets/productos/'+item.id+'.jpg" alt="Product" /><img class="thumb-secondary" src="./assets/productos/'+item.id+'.jpg" alt="Product" /></a></div><div class="product-item__content mt-3"><h4 class="title text-center"><a href="detail.html?id='+item.id+'">'+item.nombre+'</a></h4></div></div>';
-        const fav = '<div class="product-item"><div class="product-item__thumb"><a href="detail.html?id='+item.id+'"><img class="thumb-primary" src="./assets/productos/'+item.id+'.jpg" alt="Product" /><img class="thumb-secondary" src="./assets/productos/'+item.id+'.jpg" alt="Product" /></a></div><div class="product-item__content mt-3"><div class="ratting"><span><i class="ion-android-star"></i></span><span><i class="ion-android-star"></i></span><span><i class="ion-android-star"></i></span><span><i class="ion-android-star"></i></span><span><i class="ion-android-star"></i></span></div><h4 class="title text-center"><a href="detail.html?id='+item.id+'">'+item.nombre+'</a></h4></div></div>';
-        if (item.fav === 1) {
-            $('#fav').append(fav);
-        }
-        $('#'+item.categoria).append('<div class="col">'+element+'</div>');
-        cont++;
+$.get("./assets/php/listado.php", (data) => {
+  let cont = 1;
+  for (const item of data) {
+    const element = `<div class="product-item">
+    <div class="product-item__thumb">
+            <a href="detail.html?id=${item.id}">
+                <img class="thumb-primary" src="./assets/productos/${item.id}.jpg" alt="Product" />
+                <img class="thumb-secondary" src="./assets/productos/${item.id}.jpg" alt="Product" />
+            </a>
+        </div>
+        <div class="product-item__content mt-3">
+            <h4 class="title text-center">
+                <a href="detail.html?id=${item.id}">${item.nombre}</a>
+            </h4>
+            <div class="row">
+                <div class="col">
+                    <div class="ratting">
+                        <span><i class="ion-android-star"></i></span>
+                        <span><i class="ion-android-star"></i></span>
+                        <span><i class="ion-android-star"></i></span>
+                        <span><i class="ion-android-star"></i></span>
+                        <span><i class="ion-android-star-half"></i></span>
+                    </div>
+                </div>
+                <div class="col">
+                    <a href="detail.html?id=${item.id}"><button class="btn-add-to-cart"><i class="ion-eye"></i></button></a>
+                </div>
+            </div>
+        </div>
+    </div>`;
+    const fav = `<div class="product-item">
+    <div class="product-item__thumb">
+            <a href="detail.html?id=${item.id}">
+                <img class="thumb-primary" src="./assets/productos/${item.id}.jpg" alt="Product" />
+                <img class="thumb-secondary" src="./assets/productos/${item.id}.jpg" alt="Product" />
+            </a>
+        </div>
+        <div class="product-item__content mt-3">
+            <h4 class="title text-center">
+                <a href="detail.html?id=${item.id}">${item.nombre}</a>
+            </h4>
+            <div class="row">
+                <div class="col">
+                    <div class="ratting">
+                        <span><i class="ion-android-star"></i></span>
+                        <span><i class="ion-android-star"></i></span>
+                        <span><i class="ion-android-star"></i></span>
+                        <span><i class="ion-android-star"></i></span>
+                        <span><i class="ion-android-star-half"></i></span>
+                    </div>
+                </div>
+                <div class="col">
+                    <a href="detail.html?id=${item.id}"><button class="btn-add-to-cart"><i class="ion-eye"></i></button></a>
+                </div>
+            </div>
+        </div>
+    </div>`;
+    if (item.fav === 1) {
+      $("#fav").append(fav);
     }
+    $("#" + item.categoria).append('<div class="col">' + element + "</div>");
+    cont++;
+  }
 
-    // Best Product Slider Js
-    $(".product-carousel").slick({
-        slidesToShow: 5,
-        slidesToScroll: 2,
-        arrows: true,
-        autoplay: true,
-        autoplaySpeed: 5000,
-        nextArrow: '<button class="slick-next"><i class="ion-chevron-right"></i></button>',
-         prevArrow: '<button class="slick-prev"><i class="ion-chevron-left"></i></button>',
-        responsive: [{
-                breakpoint: 480,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                }
-            },
-            {
-                breakpoint: 767,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 1
-                }
-            },
-            {
-                breakpoint: 992,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 1
-                }
-            },
-            {
-                breakpoint: 1300,
-                settings: {
-                    slidesToShow: 4,
-                    slidesToScroll: 1
-                }
-            }
-        ]
-    });
+  // Best Product Slider Js
+  $(".product-carousel").slick({
+    slidesToShow: 5,
+    slidesToScroll: 2,
+    arrows: true,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    nextArrow:
+      '<button class="slick-next"><i class="ion-chevron-right"></i></button>',
+    prevArrow:
+      '<button class="slick-prev"><i class="ion-chevron-left"></i></button>',
+    responsive: [
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 1300,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
 });
 
 const validateEmail = (email) => {
-    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
-}
+  const re =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+};
 
 const goSection = (id) => {
-    $('html, body').animate({
-        scrollTop: $("#"+id).offset().top
-    }, 2000)
+  $("html, body").animate(
+    {
+      scrollTop: $("#" + id).offset().top,
+    },
+    2000
+  );
 };
